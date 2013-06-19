766cd660055ce48ade79ea29126c0f759cc2c066
Travis Martin <nitramsivart@gmail.com>
2013-06-19 16:47:59 -0400

    cleaned up some code

--- test/karate.py
+++ test/karate.py
@@ -1,6 +1,5 @@
 import networkx as nx
 import numpy as np
-#from bp_function import BP_infer as bp
 from beliefprop import bp
 import matplotlib.pylab as plt
 
@@ -14,10 +13,6 @@ omega =[(0.25,0.04),(0.04,.25)]
 tmax = 60
 ass,phi,gamma,omega,messages = bp(gamma,omega,A,tmax)
 
-print ass
-#print phi
-print gamma
-print omega
 for u in range(5):
   s = ""
   for v in range(5):
@@ -26,15 +21,6 @@ for u in range(5):
       s += "%f, " % messages[u][v][r]
     s += '\n'
   print s,
-'''
-norm  =ass[0]
-for i in range(n):
-    if ass[i]==norm:
-        ass[i]=0
-    else:
-        ass[i]=1
-
-'''
 
 nx.draw(G,node_color =ass)
 plt.show()
